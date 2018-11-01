16c32be71677bfe6b406998394243347d921ba1e
Jin <jinseo10@gmail.com>
2018-11-01 13:50:15 -0400

    Working page switch

---
+++ NESDtest/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Alert, Button, AppRegistry, Image, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import HomeScreen from './screens/HomeScreen'
+import CityServices from './screens/CityServices'
+import CommunityInformation from './screens/CommunityInformation'
+import SocialMedia from './screens/SocialMedia'
+
+export default class app extends React.Component {
+  render() {
+    return (
+      <AppStackNavigator/>
+    );
+  }
+}
+
+const AppStackNavigator = createStackNavigator({
+  Home:{
+    screen:HomeScreen
+  },
+  City:{
+    screen:CityServices
+  },
+  Community:{
+    screen:CommunityInformation
+  },
+  Social:{
+    screen:SocialMedia
+  }
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3d87ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#e2edff'
+  },
+  buttonText: {
+    padding: 20,
+    color: 'black'
+  },
+  image:{
+    alignItems: 'center',
+    width: 167,
+    height: 44
+  }
+});
---
+++ NESDtest/screens/SocialMedia.js
@@ -0,0 +1,50 @@
+import React, { Component} from 'react';
+import { Alert, Button, AppRegistry, Image, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
+
+class SocialMedia extends Component{
+    render(){
+            return(
+                <View style={styles.container}>
+                    <View style={styles.image}>
+                        <Image source={require('./image/logo.png')} />
+                    </View>
+                    
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity onPress={()=>this.props.navigation.navigate('Home')}>
+                            <View style={styles.button}>
+                                <Text style={styles.buttonText}>Go To Home Screen</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            )
+    }
+}
+export default SocialMedia;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#3d87ff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    buttonContainer: {
+      margin: 20
+    },
+    button: {
+      marginBottom: 30,
+      width: 260,
+      alignItems: 'center',
+      backgroundColor: '#e2edff'
+    },
+    buttonText: {
+      padding: 20,
+      color: 'black'
+    },
+    image:{
+      alignItems: 'center',
+      width: 167,
+      height: 44
+    }
+  });
